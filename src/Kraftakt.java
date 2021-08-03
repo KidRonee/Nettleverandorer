5c40f6d7acd15227e536adf3414aeb07a6599a90
kidronee <mathias-b@hotmail.no>
2021-08-03 15:43:34 +0200

    added changes

--- src/Kraftakt.java
+++ src/Kraftakt.java
@@ -1,6 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Kraftakt {
     private ArrayList<String> aktorer = new ArrayList<>();
     private ArrayList<Integer> postNr = new ArrayList<>();
+
+    public ArrayList reader() {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("Kraftleverandorer.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while(br)
+
+    }
 }
+
+
