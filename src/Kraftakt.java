3c4af9de2f3092cc58558d58ba89cf5ae168d6d5
KidRonee <mathias-b@hotmail.no>
2021-08-04 22:53:38 +0200

    added changes

--- src/Kraftakt.java
+++ src/Kraftakt.java
@@ -1,45 +1,28 @@
-<<<<<<< HEAD
+
 import java.io.File;
 import java.io.FileNotFoundException;
-=======
-import java.io.*;
->>>>>>> 85e5a0f67367234e565a6a5de0909f96e4ff7431
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Kraftakt {
-    private ArrayList<String> aktorer = new ArrayList<>();
-    private ArrayList<Integer> postNr = new ArrayList<>();
+    public  ArrayList<String> aktorer = new ArrayList<>();
+    private  ArrayList<Integer> postNr = new ArrayList<>();
 
     public Kraftakt() {
-<<<<<<< HEAD
-        readFromFile();
-    }
-
-    public void readFromFile() {
-        File myFile = new File("C:\\Users\\Mathi\\IdeaProjects\\Nettleverandorer\\src\\Kraftleverandorer.txt");
-        try {
-            Scanner sc = new Scanner(myFile);
-            while(sc.hasNextLine()) {
-=======
         reader();
     }
 
-    public void reader() {
-        try (Scanner sc = new Scanner(new File("C:\\Users\\TM61\\git\\src\\Kraftleverandorer.txt"))) {
+    public ArrayList<String> reader() {
+        try (Scanner sc = new Scanner(new File("src\\Kraftleverandorer.txt"))) {
             while (sc.hasNextLine()) {
->>>>>>> 85e5a0f67367234e565a6a5de0909f96e4ff7431
                 aktorer.add(sc.nextLine());
             }
             sc.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-<<<<<<< HEAD
             System.out.println("error");
-=======
->>>>>>> 85e5a0f67367234e565a6a5de0909f96e4ff7431
         }
-        System.out.println(aktorer);
+        return aktorer;
     }
 }
 
